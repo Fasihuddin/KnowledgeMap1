3d38e57408928e033ba3d69cac7537399668a8b6
bluesheeva <bluesheeva@yahoo.com>
2013-05-01 10:51:27 +1000

    1/05/2013

--- WebApp/StdQuestionsForm.aspx
+++ WebApp/StdQuestionsForm.aspx
@@ -12,42 +12,49 @@
     
         <table style="width:100%;">
             <tr>
-                <td>&nbsp;</td>
-            </tr>
-            <tr>
-                <td>
-                    <asp:DataList ID="DataList1" runat="server">
-                        <FooterTemplate>
-                            <asp:Button ID="btnSubmit" runat="server" Text="Submit" />
-                        </FooterTemplate>
-                        <HeaderTemplate>
-                            <asp:Label ID="Label1" runat="server" Font-Bold="True" Text="Answer the following questions:"></asp:Label>
-                        </HeaderTemplate>
-                        <ItemTemplate>
-                            <asp:Label ID="lblQuestionID" runat="server" Text='<%# Eval("QuestionID") %>'></asp:Label>
-                            <asp:Label ID="lblQuestion" runat="server" Text='<%# Eval("Question") %>'></asp:Label>
-                            <asp:RadioButtonList ID="RadioButtonList1" runat="server">
-                            </asp:RadioButtonList>
-                            <asp:CheckBoxList ID="CheckBoxList1" runat="server">
-                            </asp:CheckBoxList>
-                            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
-                            <br />
-                            <asp:HiddenField ID="HiddenField1" runat="server" Value='<%# Eval("AnswerType") %>' />
-                            <br />
-                        </ItemTemplate>
-                    </asp:DataList>
+                <td>Questions To Answer:<br />
                 </td>
             </tr>
             <tr>
                 <td>
-                    <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
+                    <asp:DataList ID="DataList1" width="80%" runat="server" DataSourceID="SqlDataSource" onitemdatabound="DataList1_ItemDataBound" BackColor="White" BorderColor="#999999" BorderStyle="None" BorderWidth="1px" CellPadding="3" GridLines="Vertical" >
+                        <AlternatingItemStyle BackColor="#DCDCDC" />
+                        <FooterStyle BackColor="#CCCCCC" ForeColor="Black" />
+                        <FooterTemplate>
+                            <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+                        </FooterTemplate>
+                        <HeaderStyle BackColor="#000084" Font-Bold="True" ForeColor="White" />
+                        <HeaderTemplate>
+                            <asp:Label ID="Label1" runat="server" Font-Bold="True" Text="Answer the following questions:"></asp:Label>
+                        </HeaderTemplate>
+                        <ItemStyle BackColor="#EEEEEE" ForeColor="Black" />
+                        <ItemTemplate>
+                            <asp:Label ID="lblQuestionID" runat="server" Text='<%# Eval("Question_Id") %>'></asp:Label>
+                            .&nbsp;
+                            <asp:Label ID="lblQuestion" runat="server" Text='<%# Eval("Text") %>'></asp:Label>
+                            <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+                            </asp:RadioButtonList>
+                            &nbsp;
+                        </ItemTemplate>
+                        <SelectedItemStyle BackColor="#008A8C" Font-Bold="True" ForeColor="White" />
+                    </asp:DataList>
                 </td>
             </tr>
         </table>
         <br />
+        <asp:Label ID="lblThanks" runat="server" Text="Label"></asp:Label>
+        <br />
         <br />
     
     </div>
+        <asp:SqlDataSource ID="SqlDataSource" runat="server" ConnectionString="<%$ ConnectionStrings:connString %>"
+                 SelectCommand="SELECT Questions.Question_Id, Text 
+                                FROM Test_questions JOIN Questions ON Test_questions.Question_Id = Questions.Question_Id WHERE (Test_questions.Test_Id = @TestID)">
+         
+            <SelectParameters>
+                <asp:QueryStringParameter Name="TestID" QueryStringField="id" Type="Int32" />
+            </SelectParameters>
+        </asp:SqlDataSource>
     </form>
 </body>
 </html>
--- WebApp/StdQuestionsForm.aspx.cs
+++ WebApp/StdQuestionsForm.aspx.cs
@@ -5,27 +5,136 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
+using System.Data;
 
 public partial class StdQuestionsForm : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
     }
 
-    protected void DataList1_ItemDataBound(object sender, System.Web.UI.WebControls.DataListItemEventArgs e)
+    protected void DataList1_ItemDataBound(object sender, System.Web.UI.WebControls.DataListItemEventArgs e)  
     {
         if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
         {
-            HiddenField ansType = e.Item.FindControl("HiddenField1");
-            Label questionId = e.Item.FindControl("lblQuestionID");
-            RadioButtonList rbl = e.Item.FindControl("RadioButtonList1");
-            CheckBoxList cbl = e.Item.FindControl("CheckBoxList1");
-            TextBox txt = e.Item.FindControl("TextBox1");
-            //DataSet ds = 
+            //Get the questionID from the aspx label
+            Label questionId = (Label) e.Item.FindControl("lblQuestionID");
+            RadioButtonList rbl = (RadioButtonList) e.Item.FindControl("RadioButtonList1");
+
+            //get all choices for the question
+            DataSet ds = getQuestionChoices(questionId.Text);
+
+            //bind choices to radio button lists
+            rbl.Visible = true;
+            rbl.DataSource = ds;
+            rbl.DataTextField = "Text";
+            rbl.DataValueField = "Choice_Id";
+            rbl.DataBind();
 
         }
     }
 
+    /*
+     * Method used for getting all choices of the question. It asks for questionId to be supplied
+     * */
+    private DataSet getQuestionChoices(string questionId)
+    {
+        SqlConnection conStr = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["connString"].ConnectionString);
+        DataSet ds = new DataSet();
+        try
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Choices where Question = @id", conStr);
+            SqlParameter p1 = new SqlParameter("@id", questionId);
+            cmd.Parameters.Add(p1);
+            SqlDataAdapter dtAdapt = new SqlDataAdapter();
+            dtAdapt.SelectCommand = cmd;
+            dtAdapt.Fill(ds, "choices");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
 
+        return ds;
+    }
+
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        foreach (DataListItem item in DataList1.Items)
+        {
+            if (item.ItemType == ListItemType.Item | item.ItemType == ListItemType.AlternatingItem)
+            {
+                int questionid = 0;
+                int choiceid = 0;
+                Label lbl = (Label) item.FindControl("lblQuestionID");
+                questionid = Convert.ToInt32(lbl.Text);
+
+                RadioButtonList rbl = (RadioButtonList)item.FindControl("RadioButtonList1");
+                choiceid = Convert.ToInt32(rbl.SelectedValue);
+                
+                int testId = Convert.ToInt32(Request.QueryString["id"]);
+                SaveAnswer(questionid, choiceid, testId);
+            }
+        }
+        DataList1.Visible = false;
+        lblThanks.Text = "Thank you for answering the questions!";
+    }
+
+
+    private void SaveAnswer(int qid, int cid, int testId)
+    {
+        //testId, qId, cId,answer
+ 
+        SqlConnection conStr = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["connString"].ConnectionString);
+        try
+        {
+            conStr.Open();
+            int isRight = 0;
+            //Get the answer for the question
+            SqlCommand cmd = new SqlCommand("Select Choice_Id From Choices where Question = @qId and Status LIKE 'Y'", conStr);
+            SqlParameter p1 = new SqlParameter("@qid", qid);
+            cmd.Parameters.Add(p1);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                //if student choice is similar with the answer of the question, student's answer is right
+                if(cid == reader.GetInt32(0)){
+                    isRight = 1;
+                }
+                
+            }
+            conStr.Close();
+
+            conStr.Open();
+            //Now inserting the values to database
+            cmd = new SqlCommand("insert into Student_answer(Test_Id,Question_Id,Student_choice,IsRight) values(@testId,@qid,@cid,@isRight)", conStr);
+            SqlParameter p2 = new SqlParameter("@testid", testId);
+            SqlParameter p3 = new SqlParameter("@qid", qid);
+            SqlParameter p4 = new SqlParameter("@cid", cid);
+            SqlParameter p5 = new SqlParameter("@isRight", isRight);
+            cmd.Parameters.Add(p2);
+            cmd.Parameters.Add(p3);
+            cmd.Parameters.Add(p4);
+            cmd.Parameters.Add(p5);
+
+            cmd.ExecuteNonQuery();
+            conStr.Close();
+
+
+            //SqlCommand cmd = new SqlCommand("insert into Student_answer(QuestionID,Student_choice,IsRight) values(@qid,@cid,@ct)", conStr);
+            //SqlParameter p1 = new SqlParameter("@qid", qid);
+            //SqlParameter p2 = new SqlParameter("@cid", (cid == 0 ? DBNull.Value : cid));
+            //SqlParameter p3 = new SqlParameter("@ct", (string.IsNullOrEmpty(ct) ? DBNull.Value : ct));
+            //cmd.Parameters.Add(p1);
+            //cmd.Parameters.Add(p2);
+            //cmd.Parameters.Add(p3);
+            //conStr.Open();
+            //cmd.ExecuteNonQuery();
+            //conStr.Close();
+        }catch(Exception ex){
+            Console.WriteLine(ex.ToString());
+        }
+    }
 }
